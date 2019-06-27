e47cf598618ff433b1deca72f8cb4f9c701e11ad
Jonathan Alvarez <alvarezjohnny3@gmail.com>
2019-06-27 16:18:59 -0400

    styled all products

--- server.rb
+++ server.rb
@@ -18,5 +18,20 @@ get "/cakes" do
 end
 
 get "/events" do
+  @pub_key = 'A3VSYKHFVVD6KPRIT4VF'
+  event = HTTParty.get("https://www.eventbriteapi.com/v3/events/search/?q=baking&token=#{@pub_key}")
+  JSON.parse(event)
+  @events = event['events']
+
+  @events.take(9).each do |i|
+    [i]['name']['text']
+    [i]['description']['text']
+    [i]['url']
+    [i]['start']['timezone']
+    [i]['start']['local']
+    [i]['logo']
+  end
+
+
   erb :events
 end
